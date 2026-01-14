7e76a7719e7999db0e57597d035737dc1a2fb76e
LeiO <litaosheng@ama-auto.com>
2026-01-14 17:54:30 +0800

    feat: add turn server config

--- constants/webrtc.ts
+++ constants/webrtc.ts
@@ -4,4 +4,21 @@ export const WEBRTC_ICE_SERVERS = [
   { urls: 'stun:stun2.l.google.com:19302' },
   { urls: 'stun:stun3.l.google.com:19302' },
   { urls: 'stun:stun4.l.google.com:19302' },
+  /**
+   * add the TURN server, because of the symmetric NAT, 
+   * the TURN server becomes an indispensable part, 
+   * and the probability of successful P2P connection with STUN alone is very small.
+   * 
+   * 1. this is very important unless the devices are all in the same LAN.
+   */
+  {
+    urls: 'turn:your-server-ip:3478',
+    username: '*',
+    credential: '*',
+  },
+  {
+    urls: 'turns:your-server-ip:5349',
+    username: '*',
+    credential: '*',
+  },
 ];
